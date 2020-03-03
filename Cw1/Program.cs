79805d416eb16d681111c02bb4c6b5d0d260ba3b
Jan Przystał <s19171@pjwstk.edu.pl>
2020-03-03 11:41:01 +0100

    init

---
+++ Cw1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //int? tmp1 = 1;
+            //double tmp2 = 2.0;
+            //string tmp3 = "XD";
+            //bool tmp4 = true;
+
+            //var tmp5 = "KK";
+            //var tmp6 = "i cos";
+
+            //var tmp7 = 2;
+
+            //var path = @"C:\Users\s19171\Desktop\Cw1";
+
+            //Console.WriteLine($"{tmp5} {tmp6} {tmp1+tmp7}");
+
+            //var newPerson = new Person { FirstName = "Ala" };
+
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            //var httpClient = new HttpClient();
+
+            using (var httpClient = new HttpClient())
+            {
+
+                var response = await httpClient.GetAsync(url);
+
+                //status code: 2xx
+                if (response.IsSuccessStatusCode)
+                {
+                    var htmlContent = await response.Content.ReadAsStringAsync();
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var matches = regex.Matches(htmlContent);
+
+                    foreach (var match in matches)
+                    {
+                        Console.WriteLine(match.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
